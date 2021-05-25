d5b8fcf29e76e5f960c199f7908a31a875d6c730
Ángel De La Cruz <angeldelacruzdev@gmail.com>
2021-05-24 17:58:42 -0700

    first commit

---
+++ README.txt
@@ -0,0 +1,29 @@
+--[[ ===================================================== ]]--
+--[[                ┅ ─ ⊹⊰⋌ Project ⋌⊰⊹ ─ ┅               ]]--
+--[[                ┅┅ ── ⊹⊰⋌ Zer0 ⋌⊰⊹ ── ┅┅              ]]--
+--[[ ===================================================== ]]--
+
+Project0_Realparking
+Version 1.0.0
+Hecho por 🔨Project Zer0🔨
+
+Discord:  / https://discord.gg/xgfHnxXssE
+
+2. Importar el archivo .sql a tu base de datos. 
+3. Activar en tu server.cfg: 
+start Project0_Realparking
+
+¡No distribuya ni copie sin el permiso de Project Zer0 Team
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ locales/en.lua
@@ -0,0 +1,15 @@
+Locales['en'] = {
+	["only_allow_car"]   = "Solo puedes aparcar aqui",
+	["car_saved"]        = "Tu coche ha sido aparcado",
+	["press_to_save"]    = "Presiona ~INPUT_CONTEXT~ para aparcar tu coche",
+	["not_your_car"]     = "Tienes que ser dueño del coche para aparcarlo.",
+	["need_parking_fee"] = "Presiona ~INPUT_CONTEXT~ para pagar las tarifa de parking ~g~$%s~s~",
+	["not_enough_money"] = "No tienes suficiente dinero",
+	["parking_fee"]      = "Tarifas de parking: ~g~$%s/dia",
+	["pay_success"]      = "Pagaste la tarifa de parking por ~g~$%s~s~ conduce con cuidado",
+	["parking_full"]     = "Este parking está lleno, no puedes estacionar aquí",
+	["invalid_car"]      = "No aparcaste el automóvil aquí, o no tiene permiso para conducir este vehiculo",
+	["already_parking"]  = "Este aparcamiento ya tiene almacenado un coche con la misma placa.",
+	["owner"]            = "Dueño: ~y~%s~s~",
+	["plate"]            = "Matricula: ~g~%s~s~",
+}
